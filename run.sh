99442d9b9b315713dd939a10713bb4efd3e96bbc
kumargan <ganesh.kumar@timeinc.com>
2018-04-18 08:58:20 +0530

    reuse accounts in metamask

--- run.sh
+++ run.sh
@@ -1,22 +1,23 @@
 #!bin bash
-unset DOCKER_HOST
+	unset DOCKER_HOST
 
-file="process.pid"
-if [ -f "$file" ]
-then
-	docker stop `cat process.pid`
-fi
-cp /dev/null process.pid
+	file="process.pid"
+	if [ -f "$file" ]
+	then
+		docker stop `cat process.pid`
+	fi
+	cp /dev/null process.pid
 
-docker images | grep trufflesuite/ganache-cli
-if [ $? -ne 0 ]
-then
-  echo "run the following commands to initialize the setup, and run the script again "
-  echo "git clone https://github.com/trufflesuite/ganache-cli.git && cd ganache-cli"
-  echo "docker build -t trufflesuite/ganache-cli ."
-  exit 0
-else
-  docker run -d -p 8545:8545 trufflesuite/ganache-cli:latest -a 3 -u 0 -u 1 -u 2 > process.pid
+	docker images | grep trufflesuite/ganache-cli
+	if [ $? -ne 0 ]
+	then
+	  #echo "run the following commands to initialize the setup, and run the script again "
+	  #echo "git clone https://github.com/trufflesuite/ganache-cli.git && cd ganache-cli"
+	  #echo "docker build -t trufflesuite/ganache-cli ."
+		docker pull trufflesuite/ganache-cli
+	fi
+
+  docker run -d -p 8545:8545 trufflesuite/ganache-cli:latest -m "limb away mix group together purchase because duck wonder bracket picnic subway" -a 3 -u 0 -u 1 -u 2 > process.pid
   sleep 10
   echo "use the following private keys for metamask, import the keys in metamask to get the unlocked accounts"
   docker logs `cat process.pid` | grep -n "(0)" | grep "11:(0)"
@@ -28,4 +29,3 @@ else
   echo "starting node server"
   npm i
 	npm start
-fi
