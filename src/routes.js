e3d3938889586094040e77d14ff339357885f6a1
g-shukla <gunjan.shukla@timeinc.com>
2018-04-19 14:14:05 +0530

    Fixed issue

--- src/routes.js
+++ src/routes.js
@@ -8,13 +8,14 @@ import CreateRequest from './CreateRequest';
 import LenderRequests from './LenderRequests';
 import BorrowerRequests from './BorrowerRequests';
 import App from './App';
+import CreateBorrower from './CreateBorrower';
 
 export default (
     <Router>
         <Route path="/" component={App}>
             <Route path='/lenders' component={Lenders} />
             <Route path='/create/request' component={CreateRequest} />
-            <Route path='/create/borrower' component={CreateLender} />
+            <Route path='/create/borrower' component={CreateBorrower} />
             <Route path='/create/lender' component={CreateLender} />
             <Route path='/lender/requests' component={LenderRequests} />
             <Route path='/borrower/requests' component={BorrowerRequests} />
