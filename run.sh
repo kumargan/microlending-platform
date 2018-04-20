2cdd022c6689265148bf010b4115af20f1e36222
kumargan <ganesh.kumar@timeinc.com>
2018-04-20 11:55:48 +0530

    increase gas limit

--- run.sh
+++ run.sh
@@ -36,8 +36,8 @@
 	  #echo "docker build -t trufflesuite/ganache-cli ."
 		docker pull trufflesuite/ganache-cli
 	fi
-
-  docker run -d -p 8545:8545 trufflesuite/ganache-cli:latest -m "limb away mix group together purchase because duck wonder bracket picnic subway" -a 3 -u 0 -u 1 -u 2 > process.pid
+	npm i
+  docker run -d -p 8545:8545 trufflesuite/ganache-cli:latest -l 1000000000000000000 -m "limb away mix group together purchase because duck wonder bracket picnic subway" -a 3 -u 0 -u 1 -u 2 > process.pid
   sleep 10
   echo "use the following private keys for metamask, import the keys in metamask to get the unlocked accounts"
   docker logs `cat process.pid` | grep -n "(0)" | grep "11:(0)"
@@ -47,5 +47,4 @@
   docker logs `cat process.pid` | grep "Mnemonic:" > mnemonic.txt
   mnemonic=`cat mnemonic.txt` node deploy.js
   echo "starting node server"
-  npm i
 	npm start
